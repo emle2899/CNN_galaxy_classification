5eebaf0eb1c000014367968b365b65036505ed14
Emily Levis <emily@Angelas-MacBook-Air.local>
2018-12-11 00:27:42 -0700

    output and eda update5

--- output.py
+++ output.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
     paths = 'data/image_data/'
 
     # Allowing 70% agreement to classify galaxies
-    df = targets(df, p = 0.70)
-    df_new = df.filter(['GalaxyID','labels'], axis=1)
-    images, id_num = read_images(paths)
-
-    df_id = pd.DataFrame({'GalaxyID': id_num})
-    data = pd.merge(df_id, df_new, on = 'GalaxyID', how = 'inner')
-    data = data.dropna()
-    data.to_csv('data/galaxy_labels.csv', index=False)
+    # df = targets(df, p = 0.70)
+    # df_new = df.filter(['GalaxyID','labels'], axis=1)
+    # images, id_num = read_images(paths)
+    #
+    # df_id = pd.DataFrame({'GalaxyID': id_num})
+    # data = pd.merge(df_id, df_new, on = 'GalaxyID', how = 'inner')
+    # data = data.dropna()
+    # data.to_csv('data/galaxy_labels.csv', index=False)
 
     df_lbl = pd.read_csv('data/galaxy_labels.csv')
     train_val_holdout('data/image_data', 'data', df_lbl)
