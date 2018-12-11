bc1e9e13088a3844b5b55acb277c63ba274d01aa
Emily Levis <emily@Angelas-MacBook-Air.local>
2018-12-11 00:15:46 -0700

    clean data in output

--- output.py
+++ output.py
@@ -8,14 +8,15 @@ if __name__ == '__main__':
     df = pd.read_csv('data/training_solutions_rev1.csv')
     paths = 'data/image_data/'
 
-    # Allowing 70% agreement to classify galaxies
-    # df = targets(df, p = 0.70)
-    # df_new = df.filter(['GalaxyID','labels'], axis=1)
-    # images, id_num = read_images(paths)
-    #
-    # df_id = pd.DataFrame({'GalaxyID': id_num})
-    # data = pd.merge(df_id, df_new, on = 'GalaxyID', how = 'inner')
-    # data.to_csv('data/galaxy_labels.csv', index=False)
+    Allowing 70% agreement to classify galaxies
+    df = targets(df, p = 0.70)
+    df_new = df.filter(['GalaxyID','labels'], axis=1)
+    images, id_num = read_images(paths)
+
+    df_id = pd.DataFrame({'GalaxyID': id_num})
+    data = pd.merge(df_id, df_new, on = 'GalaxyID', how = 'inner')
+    data = data.dropna()
+    data.to_csv('data/galaxy_labels.csv', index=False)
 
     df_lbl = pd.read_csv('data/galaxy_labels.csv')
     train_val_holdout('data/image_data', 'data', df_lbl)
